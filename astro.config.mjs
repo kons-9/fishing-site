7b52466d7e4b40afdac6778369d0c404a2fa49dc
kons-9 <gotoshi.euphonium@gmail.com>
2026-05-03 20:17:40 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kons-9.github.io',
+  base: '/fishing-site',
+});
